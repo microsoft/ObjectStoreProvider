aa8c84b38cfacdad95700dc741b0b3d09b2d1ba8
Thomas Tay <thomastayac@gmail.com>
2021-08-09 17:41:38 -0700

    Create unit tests and an automated testing generator to catch regressions (#27)
    Added unit tests for the B Tree, this is located in SortedBTree.spec.ts
    Added a test generator that can detect errors in the sorted-btree library through random testing. In fact, this test generator is what generated the unit tests. If there are future errors, the test generator can create additional unit tests to prevent regressions.
    Details are in the README in the folder.
    
    We will run both of them as part of the usual test suite.
    
    Other things:
    3. Updated the PR build to not generate tags (this was causing problem if we have multiple PRs in flight)
    4. Export the OrderedMapType as a type, so we can use it in TMP
    
    Co-authored-by: Thomas Tay <Tay.Thomas@microsoft.com>

--- karma.browser.conf.js
+++ karma.browser.conf.js
@@ -3,7 +3,10 @@ module.exports = (config) => {
     // 1. Load this karma plugin
     frameworks: ["mocha"],
 
-    files: [{ pattern: "dist/ObjectStoreProvider.spec.js" }],
+    files: [
+      { pattern: "dist/ObjectStoreProvider.spec.js" },
+      { pattern: "dist/SortedBTree.spec.js" },
+    ],
     customLaunchers: {
       FirefoxHeadless: {
         base: "Firefox",
---
+++ src/tests/sorted-btree/utils.ts
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ * =================================================================
+ * Random utils that don't really fit anywhere
+ */
+
+export const DEFAULT_COMPARATOR = (a: number, b: number) =>
+  a < b ? -1 : a === b ? 0 : 1;
+export const REVERSE_COMPARATOR = (a: number, b: number) =>
+  a > b ? -1 : a === b ? 0 : 1;
+
+export function isWithinBounds(arr: any[], i: number) {
+  return i >= 0 && i < arr.length;
+}
+
+export function unreachable() {
+  throw new Error("Unreachable");
+}
+
+export type TestingComparator = (a: number, b: number) => 1 | 0 | -1;
+
+export interface ISortedTestingMap {
+  get(k: number): number | undefined;
+
+  getIndex(
+    index: number,
+    isReversed: boolean,
+    startKey: number
+  ): [number, number] | undefined;
+
+  set(k: number, v: number): boolean;
+
+  remove(k: number): boolean;
+
+  get size(): number;
+}
