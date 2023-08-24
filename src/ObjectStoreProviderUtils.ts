8104b3a08726fbdf9c14d3236a5639d6ab59789f
danielmsft <daniellopez@microsoft.com>
2023-08-24 13:14:23 -0700

    making logger type an interface

--- src/ObjectStoreProviderUtils.ts
+++ src/ObjectStoreProviderUtils.ts
@@ -26,11 +26,11 @@ declare global {
   }
 }
 
-export type Logger = {
+export interface Logger {
   log(message?: any, ...optionalParams: any[]): void;
   warn(message?: any, ...optionalParams: any[]): void;
   error(message?: any, ...optionalParams: any[]): void;
-};
+}
 
 // Max array length (uint): 2^32 - 1
 export const MAX_COUNT = 4294967295;
